0cb6df6e46961490ce4ebb5ddf561346c296bf54
Rameshreddy <remeshannam32@gmail.com>
2022-09-17 13:45:07 +0530

    added new version of code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "hello welcome to rayachoty");
+		get("/Ramesh", (req, res) -> "Deploy to new version of code");
 	}
 }
